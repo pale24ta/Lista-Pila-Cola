8dacaf3c105a43607d26b40a62ae8a45593e5273
brigido <noguerabrigido@gmail.com>
2025-08-14 00:40:41 -0400

    nuevo elementos

--- Lista(C++)/taller3/main.cpp
+++ Lista(C++)/taller3/main.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include <sstream>
+#include <vector>
+#include <string>
 #include "lista.hpp"
 #include "cola.hpp"
 // #include "listaCircle.hpp"
@@ -10,17 +12,22 @@ using namespace std;
 
 
 int main(){
+	int i,j;
+	int input;
+	Lista<Lista<int>> numeros;
 
+	string leerLinea;
+	while(getline(cin,leerLinea)){
+		
+		istringstream flujo(leerLinea);
+		numeros.insertar(Lista<int>(),1); 
 
-	Lista<int> otra;
-	Lista<int> a1,a2;
-	
-	for(int i=2; i < 11; i++){
-		otra.insertar(i*2,1);
+		while(flujo>>input){
+			numeros.consultar(1).insertar(input,1);
+		}
+
 	}
 
-	otra.split(a1,a2);
-
-	cout<<a1;
+	cout<<numeros;
 	return 0;
 }
